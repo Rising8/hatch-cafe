bfe483f6f06b07eca78b7b056f9e67f42e8af363
Edmand <edmand66@hotmail.com>
2025-07-25 14:25:28 +0800

    style(footer): Fix navigation links and refine footer styling

--- footer.php
+++ footer.php
@@ -2,28 +2,29 @@
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/style/crack9.png" class="img-fluid" alt="Image description">
     <div class="container">
          <!-- Sign Up Email -->
-        <div class="container signup-email text-center pb-3">
+        <div class="container signup-email text-center pt-2 pb-3 mt-n2">
             <h2 class="footer-h2 fs-3 fs-md-2 fs-lg-1 pb-2">Don’t Miss a Crumb!</h2>
             <p class="footer-p">Be the first to know about new flavours, specials, and Hatch news. Sign up for tasty updates!</p>
-            <input type="text" class="emailbox" placeholder="Email Address:">
-            <button class="emailbutton" aria-label="Email box">Sign Up</button>
+            <div class="email-form-wrapper">
+                <input type="text" class="emailbox" placeholder="Email Address:">
+                <button class="emailbutton" aria-label="Email box">Sign Up</button>
+            </div>
         </div>
 
          <!-- Social Media Icons -->
         <div class="container text-center mb-4">
-            <a href="#" class="icons me-3"><i class="fa-brands fa-instagram"></i></a>
-            <a href="#" class="icons me-3"><i class="fa-brands fa-tiktok"></i></a>
-            <a href="#" class="icons me-3"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.instagram.com/hatch.perth/" target="_blank" rel="noopener" class="icons me-3"><i class="fa-brands fa-instagram"></i></a>
+            <p class="footer-p mb-2">Follow us on Instagram for sweet surprises 🍓</p>
         </div>
 
         <!-- Navigation Links -->
-        <div class="container text-center">
+        <div class="nav-links container text-center">
             <ul class="list-inline mb-0">
-                <li class="list-inline-item mx-2"><a href="#" class="text-white text-decoration-none">About Us</a></li>
-                <li class="list-inline-item mx-2"><a href="#" class="text-white text-decoration-none">Menu</a></li>
-                <li class="list-inline-item mx-2"><a href="#" class="text-white text-decoration-none">Gift Card</a></li>
-                <li class="list-inline-item mx-2"><a href="#" class="text-white text-decoration-none">Membership</a></li>
-                <li class="list-inline-item mx-2"><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
+                <li class="list-inline-item mx-2"><a href="<?php echo site_url('/about'); ?>" class="text-white text-decoration-none">About Us</a></li>
+                <li class="list-inline-item mx-2"><a href="<?php echo site_url('/menu'); ?>" class="text-white text-decoration-none">Menu</a></li>
+                <li class="list-inline-item mx-2"><a href="<?php echo site_url('/gift-cards'); ?>" class="text-white text-decoration-none">Gift Cards</a></li>
+                <li class="list-inline-item mx-2"><a href="<?php echo site_url('/membership'); ?>" class="text-white text-decoration-none">Membership</a></li>
+                <li class="list-inline-item mx-2"><a href="<?php echo site_url('/contact'); ?>" class="text-white text-decoration-none">Contact Us</a></li>
             </ul>
         </div>
 
